b5337662316853d769a70c26f96d5a41c7c30a7b
Fatalai <fatalaijon@gmail.com>
2020-09-07 15:02:47 +0700

    finish views and templates for part 4

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,8 @@
 """Entities in the polls application"""
-
+import datetime
 from django.db import models
+# a date that's compabible with Django DateTimeField
+from django.utils import timezone
 
 class Question(models.Model):
     """A poll question with some text, starting date, and closing date"""
@@ -10,8 +12,17 @@ class Question(models.Model):
     def __str__(self):
         """string representation of a question"""
         return self.question_text
-    
 
+    def was_published_recently(self) -> bool:
+        """Return true if question was published in the last 1 day."""
+        start_date = timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return self.pub_date >= start_date and self.pub_date <= now
+    
+    def is_current(self) -> bool:
+        """Query if this question has publication date before now"""
+        now = timezone.now()
+        return self.pub_date <= now
 
 class Choice(models.Model):
     """A possible answer to a poll question"""
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,50 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Question
 
 """Views to handle requests to the polls app"""
 
 def index(request):
-	return HttpResponse("This is the polls index page")
+	"""Show a list of current questions"""
+	now = timezone.now()
+	# pub_date__lte=now means pub_date() <= now
+	questions = Question.objects.filter( pub_date__lte=now )
+	context = {'questions': questions, }
+	return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+	"""Return a page showing details of a question"""
+	try:
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		return Http404(f"Question {question_id} does not exist")
+	context = {'question': question}
+	return render(request, 'polls/detail.html', context)
+
+def vote(request, question_id):
+	# lookup the question
+	try:
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		return Http404(f"Question {question_id} does not exist")
+	if not question.is_current():
+		return HttpResponse("Voting not allowed for that question", status=403)
+	# get the user's choice
+	try:
+		choice_id = request.POST['choice']
+	except KeyError:
+		context = {'question': question, 
+		    'error_message': "You didn't select a valid choice"}
+		return render(request, 'polls/detail.html', context)
+	try:
+		selected_choice = question.choice_set.get(pk=choice_id)
+	except Choice.DoesNotExist:
+		context = {'question': question, 
+		    'error_message': "You didn't select a valid choice"}
+		return render(request, 'polls/detail.html', context)
+	# record the vote
+	selected_choice.votes += 1
+	selected_choice.save()
+	return HttpResponseRedirect(reverse('polls:result',args=(question_id,)))
